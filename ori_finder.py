a6e05baa357acea95816080752f3c24c50cbfb80
Ethan Dayley <eedayley@gmail.com>
2020-08-19 23:20:40 -0600

    Fixed and optimized function to find repeating k-mers

--- ori_finder.py
+++ ori_finder.py
@@ -1,45 +1,65 @@
 #!/usr/bin/env python3
 
 
-import itertools
+from typing import Dict, List
 
 # this module provides functions for finding the ORI of a DNA sequence
 
-def get_top_repeated_sequences(genome: str, sequence_length: int, number_to_return: int):
+def get_top_repeated_kmers(genome: str, kmer_length: int, max_num_matches: int):
     '''
-    Runs through all possible permutations of length sequence_length,
-    returning the top number_to_return segments from the genome.
-    The list returned from the function will contain dictionaries of the following
-    format:
-    {'sequence': <sequence matched>, 'times_matched': <times matched>}
+    Finds the top repeated k-mers in the genome of length kmer_length,
+    returning a list of length <= max_num_matches
     '''
 
-    # create dictionary
-    sequences_matched = []
+    # initialize dict for storing k-mers
+    kmers: Dict[str,int] = {}
 
-    # nucleotide types
-    nucleotides = ['A', 'T', 'G', 'C']
+    # for efficiency, variable stores maximum index to loop to
+    max_box_start = len(genome) - kmer_length
 
-    # iterate through all possible combinations of sequence_length
-    for sequence in itertools.combinations_with_replacement(nucleotides, sequence_length):
-        times_matched = genome.count(''.join(sequence))
+    # loop through genome, sliding a "box" of length kmer_length along it
+    for box_start in range(max_box_start):
+        curr_kmer = genome[box_start:box_start+kmer_length]
+        if curr_kmer in kmers:
+            kmers[curr_kmer] += 1
+        else:
+            kmers[curr_kmer] = 1
 
-        if len(sequences_matched) != 0 and times_matched > sequences_matched[-1]['times_matched']:
-            for i in range(len(sequences_matched)-1, -1, -1):
-                # iterate backwards through the array, stopping when we find
-                # a sequence that's repeated more than the current one
-                if sequences_matched[i]['times_matched'] >= times_matched:
-                    # splice in the more frequent one, making sure to drop any extras off the end
-                    sequences_matched = sequences_matched[:i+1] + \
-                                        [{'sequence_matched': sequence, 'times_matched': times_matched}] + \
-                                        sequences_matched[i+1:number_to_return-1]
+    # initialize list of top matches
+    top_kmers: List[Dict] = []
+
+    # loop through kmers, creating sorted list of top matches
+    for kmer in kmers:
+        if len(top_kmers) == 0 or \
+            top_kmers[-1]['num_occurrences'] >= kmers[kmer] and len(top_kmers) < max_num_matches:
+            # list empty OR list below target length and num_ocurrences < last element
+            # NOTE: case doesn't capture equal num_ocurrences if length >= max_num_matches
+            top_kmers.append({'kmer': kmer, 'num_occurrences': kmers[kmer]})
+        elif top_kmers[-1]['num_occurrences'] <=  kmers[kmer]:
+            # loop backwards through top_kmers
+            for i in range(len(top_kmers)-1, -1, -1):
+                if top_kmers[i]['num_occurrences'] >= kmers[kmer]:
+                    # define new index (for readability)
+                    new_index = i + 1
+
+                    # break if smaller than last element
+                    if new_index >= max_num_matches:
+                        break
+
+                    # insert new value into list
+                    top_kmers = top_kmers[:new_index] \
+                        + [{'kmer': kmer, 'num_occurrences': kmers[kmer]}] \
+                        + top_kmers[new_index:max_num_matches]
                     break
-                elif sequences_matched[i]['times_matched'] < times_matched and i == 0:
-                    sequences_matched = [{'sequence_matched': sequence, 'times_matched': times_matched}] + \
-                                        sequences_matched[:number_to_return-1]
-        elif len(sequences_matched) == 0:
-            sequences_matched.append({'sequence_matched': sequence, 'times_matched': times_matched})
-    return sequences_matched
+
+                elif top_kmers[i]['num_occurrences'] < kmers[kmer] and i == 0:
+                    # insert new value into list
+                    top_kmers = [{'kmer': kmer, 'num_occurrences': kmers[kmer]}] \
+                                + top_kmers[:max_num_matches]
+                    break
+
+    return top_kmers
+
 
 
 if __name__ == '__main__':
@@ -47,5 +67,5 @@ if __name__ == '__main__':
     genome = f.read()
     f.close()
     sequence_length = int(input('sequence length: '))
-    for i in get_top_repeated_sequences(genome, sequence_length, 10):
+    for i in get_top_repeated_kmers(genome, sequence_length, 50):
         print(i)
